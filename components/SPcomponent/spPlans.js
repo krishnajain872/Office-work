708cae10cae61f3fd4c618878b9e5bee34f09516
krishna jain <120629145+krishnajain872@users.noreply.github.com>
2022-12-29 11:50:58 +0530

    plan page getting things done

--- components/SPcomponent/spPlans.js
+++ components/SPcomponent/spPlans.js
@@ -3,12 +3,37 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import Transition from "../transitions";
 
-
 export default function Plan() {
   // const router = useRouter()
   return (
-  <div><img alt="image"/>
-       <h2></h2>
-  </div>
+    <>
+      <div>
+        <img alt="image" />
+      </div>
+      <div>
+        <h2>Mobile Protection For Asus</h2>
+        <h3>Zenfone 5 Mobile</h3>
+        <p>Best Plan For Secure Your Mobile Screen</p>
+        <p>Best Plan For Secure Your Mobile Screen</p>
+        <p>Mobile Screen Protection Plan Cover Any Kind Of Screen Damage</p>
+      </div>
+      <div>
+        <div>
+          <img alt="image1" />
+          <p>24/7 Chat Support</p>
+        </div>
+        <div>
+          <img alt="image2" />
+          <p>Free Pickup & Delivery</p>
+        </div>
+        <div>
+          <img alt="image3" />
+          <p>Authorised Service Center</p>
+        </div>
+      </div>
+      <div>
+       
+      </div>
+    </>
   );
 }
